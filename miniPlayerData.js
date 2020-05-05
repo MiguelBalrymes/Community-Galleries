addf127c5c66ebbc1a56a6fab5446de00dd516c7
Miguel Balrymes <40614454+MiguelBalrymes@users.noreply.github.com>
2020-05-05 16:48:24 +0100

    Add files via upload

---
+++ miniPlayerData.js
@@ -0,0 +1,15 @@
+klynt.miniPlayerData = {
+    "thumbnail": "Medias/Thumbnails/Norman_Rd_Gallery_wall.jpg.jpg",
+    "launchAppWording": "Then Launch Project",
+    "resumePlaybackWording": "Resume playback?",
+    "fullscreenInfoWording": "This program will launch in fullscreen",
+    "yesWording": "Yes",
+    "title": "Norman Rd Gallery",
+    "thanksForWatchingWording": "Thanks for watching!",
+    "noWording": "No",
+    "analyticsKey": "",
+    "redirectToMobileApp": "auto",
+    "downloadAppWording": "Download App",
+    "url": "",
+    "description": "A space for art during lockdoen"
+}
